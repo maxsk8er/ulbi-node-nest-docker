83fc702fb77e29443d098cd0f42c9ba6c0eaaeb5
maxsk8er <sk8er999@gmail.com>
2021-07-08 22:20:44 +0100

    prod, dev conf

--- nest-course/src/app.module.ts
+++ nest-course/src/app.module.ts
@@ -1,20 +1,27 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { env } from "process";
+import { UsersModule } from './users/users.module';
 
 @Module({
 	controllers: [],
 	providers: [],
-	imports:[
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${process.env.NODE_ENV}.env`
+		}),
 		SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'nest-course',
-      models: [],
+			dialect: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASS,
+			database: process.env.POSTGRES_DB,
+			models: [],
 			autoLoadModels: true
-    }),
+		}),
+		UsersModule,
 	]
 })
 
