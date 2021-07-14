f4ed12ae48999a058b81b43f630165d8c2772ee5
maxsk8er <sk8er999@gmail.com>
2021-07-14 21:18:41 +0100

    Ban user. Add role

--- nest-course/src/users/users.controller.ts
+++ nest-course/src/users/users.controller.ts
@@ -1,6 +1,11 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorators';
+import { RolesGuard } from 'src/auth/roles.guards';
+import { RolesModule } from 'src/roles/roles.module';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -25,9 +30,28 @@ export class UsersController {
 
 	@ApiOperation({ summary: 'List all users' })
 	@ApiResponse({ status: 200, type: [User] })
-	@UseGuards(JwtAuthGuard)
+	@Roles("ADMIN")
+	@UseGuards(RolesGuard)
 	@Get()
 	getAll() {
 		return this.usersService.getAllUsers();
+	}
+
+	@ApiOperation({summary: 'Give a role'})
+	@ApiResponse({status: 200})
+	@Roles("ADMIN")
+	@UseGuards(RolesGuard)
+	@Post('/role')
+	addRole(@Body() dto: AddRoleDto) {
+			return this.usersService.addRole(dto);
+	}
+
+	@ApiOperation({summary: 'Ban user'})
+	@ApiResponse({status: 200})
+	@Roles("ADMIN")
+	@UseGuards(RolesGuard)
+	@Post('/ban')
+	ban(@Body() dto: BanUserDto) {
+			return this.usersService.ban(dto);
 	}
 }
