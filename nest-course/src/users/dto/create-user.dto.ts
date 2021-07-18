8ec5ad6fc9f08504c5d8b738cc39bba795f1b5e5
maxsk8er <sk8er999@gmail.com>
2021-07-18 17:09:32 +0100

    Pipes. Validations of data

--- nest-course/src/users/dto/create-user.dto.ts
+++ nest-course/src/users/dto/create-user.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-
+import { IsString, Length, IsEmail } from "class-validator";
 
 export class CreateUserDto {
-	
-	@ApiProperty({example: 'user@gmail.com', description: 'Email'})
+
+	@ApiProperty({ example: 'user@gmail.com', description: 'Email' })
+	@IsString({ message: 'Must be a string' })
+	@IsEmail({}, { message: 'Not a valid email' })
 	readonly email: string;
-	@ApiProperty({example: '123456', description: 'Password'})
+	@ApiProperty({ example: '123456', description: 'Password' })
+	@IsString({ message: 'Must be a string' })
+	@Length(4, 16, { message: 'Must be smaller than 16 and bigger than 4 characters' })
 	readonly password: string;
 }
--- nest-course/src/users/users.controller.ts
+++ nest-course/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Roles } from 'src/auth/roles-auth.decorators';
@@ -11,17 +11,14 @@ import { User } from './users.model';
 import { UsersService } from './users.service';
 
 @ApiTags('Users')
-
 @Controller('users')
 export class UsersController {
 
-	constructor(private usersService: UsersService) {
-
-	}
+	constructor(private usersService: UsersService) {}
 
 	@ApiOperation({ summary: 'Create a new user' })
 	@ApiResponse({ status: 200, type: User })
-
+	
 	@Post()
 	create(@Body() userDto: CreateUserDto) {
 		return this.usersService.createUser(userDto);
@@ -37,21 +34,21 @@ export class UsersController {
 		return this.usersService.getAllUsers();
 	}
 
-	@ApiOperation({summary: 'Give a role'})
-	@ApiResponse({status: 200})
+	@ApiOperation({ summary: 'Give a role' })
+	@ApiResponse({ status: 200 })
 	@Roles("ADMIN")
 	@UseGuards(RolesGuard)
 	@Post('/role')
 	addRole(@Body() dto: AddRoleDto) {
-			return this.usersService.addRole(dto);
+		return this.usersService.addRole(dto);
 	}
 
-	@ApiOperation({summary: 'Ban user'})
-	@ApiResponse({status: 200})
+	@ApiOperation({ summary: 'Ban user' })
+	@ApiResponse({ status: 200 })
 	@Roles("ADMIN")
 	@UseGuards(RolesGuard)
 	@Post('/ban')
 	ban(@Body() dto: BanUserDto) {
-			return this.usersService.ban(dto);
+		return this.usersService.ban(dto);
 	}
 }
